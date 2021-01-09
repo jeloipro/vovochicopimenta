61dd9d19455f7424f043c1e006cbe08b75f17b84
Sr Eloi <jeloipro@gmail.com>
2021-01-09 20:26:40 -0300

    acerto 5 09-01-2021

--- src/templates/youtube.js
+++ src/templates/youtube.js
@@ -1,8 +1,11 @@
+// https://getbootstrap.com/docs/5.0/layout/columns/
 import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import Icons from "../components/Icons"
 
 export const pageQuery = graphql`
   query YoutubeQuery($id: String!){
@@ -30,6 +33,16 @@ const YoutubePage = ({ data }) => {
 				<h1>{frontmatter.title}</h1>
 				<article dangerouslySetInnerHTML={{ __html: html }} />
 			</div>
+
+			<div className="col-6">
+				<ul class="list-group">
+					<li class="list-group-item"><Icons.Email className="w-25 text-danger" />Email</li>
+					<li class="list-group-item"><Icons.Telegram className="w-25 text-danger" />Telegram</li>
+					
+					<li class="list-group-item">Youtube</li>
+				</ul>
+			</div>
+
 		</Layout>
 	)
 }
